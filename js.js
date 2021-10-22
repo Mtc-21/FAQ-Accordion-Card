553f4e72a99280a36784989feb1c73949312871e
Martha Mendoza <71796360+Mtc-21@users.noreply.github.com>
2021-10-21 21:18:39 -0400

    Add files via upload
    Update of tag html article and children div for details and summary and settings in file js and css

--- js.js
+++ js.js
@@ -1,33 +1,13 @@
- const btn = document.querySelectorAll(".title");
-    const text = document.querySelector(".title p");
-    const seleccion = document.getElementsByClassName("selected");
-
-    const hideQuestion = () => {
-      console.log(seleccion[0].parentNode)
-      seleccion[0].style.transform = "rotate(360deg)";
-      seleccion[0].parentNode.nextElementSibling.style.display = "none";
-      seleccion[0].previousElementSibling.style.fontWeight = "400";
-      seleccion[0].className = "";
-    }
-    const showQuestion = (btn) => {
-      console.log(btn)
-      btn.children[1].className = "selected";
-      btn.children[1].style.transform = "rotate(180deg)";
-      btn.nextElementSibling.style.display = "block";
-      btn.children[0].style.fontWeight = "bold";
-    }
-
+const btn = document.querySelectorAll(".question");
     btn.forEach(btn => {
-      btn.addEventListener("click", () => {
-        if (btn.children[1].className == "selected") {
-          hideQuestion();
+      btn.addEventListener("toggle", () => {
+        console.log(btn.children[0].children[0])
+        if (btn.open) {
+          btn.children[0].children[0].style.transform = "rotate(180deg)";
+          btn.style.fontWeight = "bold";
         } else {
-          if (seleccion.length == 0) {
-            showQuestion(btn);
-          } else {
-            hideQuestion();
-            showQuestion(btn);
-          }
+          btn.children[0].children[0].style.transform = "rotate(360deg)";
+          btn.style.fontWeight = "400";
         }
       });
     });
